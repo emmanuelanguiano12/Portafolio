6eea900025adb4ad60f54680b6b53f6a895018c9
emmanuelanguiano12 <emmanuelanguiano1223@gmail.com>
2024-03-12 14:01:20 -0600

    Navbar Update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,33 +1,45 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+const NavItems = [
+  { path: "#Home", text: "Home" },
+  { path: "#Proyectos", text: "Proyectos" },
+  { path: "#About", text: "Sobre mí" },
+];
+
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-        {/* component */}
-        <nav className="flex px-3 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center">
-            <a
-              href="#Home"
-              className="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
-            >
-              Home
-            </a>
-            <a
-              href="#Proyectos"
-              className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-            >
-              Proyectos
-            </a>
-            <a
-              href="#"
-              className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-            >
-              pricing
-            </a>
-            <a
-              href="#"
-              className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
-            >
-              blog
-            </a>
-        </nav>
-    </>
+    <nav className={`flex px-3 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center ${isScrolled ? 'bg-slate-700' : ''}`}>
+      {NavItems.map((NavItem) => (
+        <a
+          key={NavItem.path}
+          href={NavItem.path}
+          className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+          {NavItem.text}
+        </a>
+      ))}
+    </nav>
   );
 };
+
